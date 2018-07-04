36b95d9941fa52898462d0cd2aec2e06ecaaedcb
fanz01 <2455633487@qq.com>
2018-07-04 17:42:46 +0800

    使用组件

--- src/modules/Output/Create/Step_2/index.js
+++ src/modules/Output/Create/Step_2/index.js
@@ -1,54 +1,66 @@
 // Created by liuliyuan on 2018/6/30
-import React,{Component} from 'react'
+import React, {Component} from 'react'
 import {withRouter} from 'react-router-dom'
-import { Button } from 'antd';
-import CustomizeTabs from '../Tabs'
+import {Button} from 'antd';
 import TabPane2 from './tab2'
+import CustomizeTabs from '../../../../components/Tabs/index'
+import TabPane1 from "../Step_1/tab1";
 
 class Step2 extends Component {
-    state={
-        updateKey:Date.now(),
-        visible:false,
-        data:{},
-    }
+  state = {
+    updateKey: Date.now(),
+    visible: false,
+    data: {},
+  }
 
-    setData = data =>{
-        this.mounted && this.setState({
-            data
-        })
-    }
+  setData = data => {
+    this.mounted && this.setState({
+      data
+    })
+  }
 
-    handleSubmit = (e) => {
-        e && e.preventDefault();
-        console.log(this.state.data)
-        this.props.history.push('/web/output/create/present')
-    }
+  handleSubmit = (e) => {
+    e && e.preventDefault();
+    console.log(this.state.data)
+    this.props.history.push('/web/output/create/present')
+  }
 
-    mounted = true;
-    componentWillUnmount(){
-        this.mounted = null;
-    }
+  mounted = true;
 
-    render(){
+  componentWillUnmount() {
+    this.mounted = null;
+  }
 
-        return(
-            <React.Fragment>
+  render() {
 
-                <CustomizeTabs
-                    tab="2"
-                    props={this.props}
-                    TabPane_2={ <TabPane2 setData={this.setData.bind(this)} /> }
-                />
+    return (
+    <React.Fragment>
 
-                <div className="steps-action">
-                    <Button type="primary" onClick={this.handleSubmit} > 下一步</Button>
-                    <Button style={{ marginLeft: 8 }}> 保存 </Button>
-                    <Button style={{ marginLeft: 8 }} href="/web/output/create/write"> 上一步 </Button>
-                </div>
-
-            </React.Fragment>
-        )
-    }
+      <CustomizeTabs
+      defaultActiveKey='1'
+      tabPaneOptions={
+        [
+          {
+            title: '产值单基本信息',
+            component: <TabPane1/>
+          },
+          {
+            title: '形象进展',
+            component: <TabPane2 setData={this.setData.bind(this)}/>
+          }
+        ]
+      }
+      stepsAction={
+        <div className="steps-action">
+          <Button type="primary" onClick={this.handleSubmit}> 下一步 </Button>
+          <Button style={{marginLeft: 8}}> 保存 </Button>
+          <Button style={{marginLeft: 8}} href="/web/output/create/write"> 上一步 </Button>
+        </div>
+      }
+      />
+    </React.Fragment>
+    )
+  }
 }
 
 export default withRouter(Step2)
--- src/modules/Output/Create/Step_3/index.js
+++ src/modules/Output/Create/Step_3/index.js
@@ -1,26 +1,42 @@
 import React from 'react'
-import CustomizeTabs from "../Tabs";
+import CustomizeTabs from '../../../../components/Tabs/index'
 import TabPane2 from "../Step_2/tab2";
 import TabPane3 from "./tab3";
 import {Button} from "antd";
+import TabPane1 from "../Step_1/tab1";
 
 
-export default class Step2 extends React.Component{
-  render(){
-    return(
+export default class Step2 extends React.Component {
+  render() {
+    return (
     <React.Fragment>
 
       <CustomizeTabs
-      tab="3"
-      TabPane_2={ <TabPane2 /> }
-      TabPane_3={ <TabPane3 /> }
+      defaultActiveKey='2'
+      tabPaneOptions={
+        [
+          {
+            title: '产值单基本信息',
+            component: <TabPane1/>
+          },
+          {
+            title: '形象进展',
+            component: <TabPane2/>
+          },
+          {
+            title: '产值明细和发票',
+            component: <TabPane3/>
+          }
+        ]
+      }
+      stepsAction={
+        <div className="steps-action">
+          <Button type="primary" href="/web/output/create/site"> 下一步 </Button>
+          <Button style={{marginLeft: 8}}> 保存 </Button>
+          <Button style={{marginLeft: 8}} href="/web/output/create/fill"> 上一步 </Button>
+        </div>
+      }
       />
-
-      <div className="steps-action">
-        <Button type="primary" href="/web/output/create/site"> 下一步 </Button>
-        <Button style={{ marginLeft: 8 }} > 保存 </Button>
-        <Button style={{ marginLeft: 8 }} href="/web/output/create/fill"> 上一步 </Button>
-      </div>
 
     </React.Fragment>
     )
